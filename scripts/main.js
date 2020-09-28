3aea3a2451bc9f6af1ac6aba3cd66634f631fb38
Danila Moiseenkov <smorgonprogger@gmail.com>
2020-09-29 00:03:01 +0300

    --amend

--- scripts/main.js
+++ scripts/main.js
@@ -16,3 +16,8 @@ function getURLParam(param) {
   let value = urlParams.get(param);
   return value;
 }
+
+logoutLink = document.querySelector('.logout-button')
+logoutLink.addEventListener("click", (e) => {
+  authService.logOut();
+}, true);
